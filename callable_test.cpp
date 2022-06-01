161e44fc166ec2bf4dd8ba31a809f901962b1233
pelectron <43130018+pelectron@users.noreply.github.com>
2022-06-01 12:55:03 +0200

    m

--- callable_test.cpp
+++ callable_test.cpp
@@ -1,4 +1,5 @@
 #include "sgl_callable2.hpp"
+#include <iostream>
 using Call = sgl::Callable2<int(int)>;
 int free_func(int a) { return 4 * a; }
 struct Struct {
@@ -8,21 +9,25 @@ struct Struct {
 };
 const auto lambda = [](int a) { return a; };
 int        main() {
-  Struct a,c;
-  auto   capture_lambda = [&a, &c](int k) { return a.b + c.b + k; };
-  Struct s{10};
-  Call   call1([](int a) { return 2 * a; });
-  Call   call2(&free_func);
-  Call   call3(s, &Struct::mfn);
-  Call   call4(s, &Struct::cmfn);
-  Call   call5(lambda);
-  Call call6(capture_lambda);
-  int b{0};
+  Struct     a, c;
+  const auto capture_lambda = [&a, &c](int k) { return a.b + c.b + k; };
+  Struct     s{10};
+  Call       call1([](int a) { return 2 * a; });
+  Call       call2(&free_func);
+  Call       call3(s, &Struct::mfn);
+  Call       call4(s, &Struct::cmfn);
+  Call       call5(lambda);
+  Call       call6(capture_lambda);
+  int        b{0};
   if (call2(1) == (2 * call1(1))) {
     b = 1;
   }
   if ((call3(2) == call4(2))) {
     b += 2;
+  }
+  if (b == 3) {
+    std::cout << "b equals three\n";
+    return call5(1) == call6(1);
   }
   return b;
 }
