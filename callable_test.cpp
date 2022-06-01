3d5f41081f8a407be730c825656379a4821c63ee
pelectron <43130018+pelectron@users.noreply.github.com>
2022-06-01 18:57:52 +0200

    m

--- callable_test.cpp
+++ callable_test.cpp
@@ -1,6 +1,6 @@
 #include "sgl_callable2.hpp"
 #include <iostream>
-using Call = sgl::Callable2<int(int)>;
+using Call = sgl::Callable<int(int)>;
 int free_func(int a) { return 4 * a; }
 struct Struct {
   int mfn(int a) { return 8 * a + b; }
@@ -10,7 +10,7 @@ struct Struct {
 const auto lambda = [](int a) { return a; };
 int        main() {
   Struct     a, c;
-  const auto capture_lambda = [&a, &c](int k) { return a.b + c.b + k; };
+  auto capture_lambda = [&a, &c](int k) { return (a.b++) + c.b + k; };
   Struct     s{10};
   Call       call1([](int a) { return 2 * a; });
   Call       call2(&free_func);
@@ -18,6 +18,9 @@ int        main() {
   Call       call4(s, &Struct::cmfn);
   Call       call5(lambda);
   Call       call6(capture_lambda);
+  if(call6(5) != call6(5)){
+    std::cout << "call6(5) != call6(5)\n";
+  }
   int        b{0};
   if (call2(1) == (2 * call1(1))) {
     b = 1;
