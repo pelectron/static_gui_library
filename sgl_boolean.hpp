0fee4d0cf6ea8c456f1dabccc1e5fe9d8ff31bf9
pelectron <43130018+pelectron@users.noreply.github.com>
2022-06-01 10:34:48 +0200

    added true and false string to Boolean_t

--- sgl_boolean.hpp
+++ sgl_boolean.hpp
@@ -12,7 +12,12 @@ namespace sgl {
   /// @endcond
 
   /**
-   * @brief This class implements a basic boolean item.
+   * @brief This class implements a basic boolean item. When clicked, it will
+   * toggle it's value and set it's text accordingly.
+   * @details Boolean_t consists of the following data:
+   * - boolean value: the value of this item
+   * - 'true' string: the text to show when value == true.
+   * - 'false' string: the text to show when value == false.
    *
    * @tparam LineWidth number of characters per line in the menu
    * @tparam CharT character type of the item
@@ -30,59 +35,62 @@ namespace sgl {
      * @brief Construct a boolean item, which toggles its text between "TRUE"
      * and "FALSE" when clicked.
      *
-     * @param item_name name of the boolean item
+     * @param name name of the boolean item
      * @param initial_value initial value of the item
      */
-    Boolean_t(StringView item_name, bool initial_value)
-        : Button_t<LineWidth, CharT>(
-              item_name,
-              initial_value ? StringView(impl::True<CharT>)
-                            : StringView(impl::False<CharT>),
-              [](Button_t<LineWidth, CharT>& button) {
-                auto& b = static_cast<Boolean_t<LineWidth, CharT>&>(button);
-                if (b.get_value()) {
-                  b.set_text(impl::False<CharT>);
-                  b.set_value(false);
-                } else {
-                  b.set_text(impl::True<CharT>);
-                  b.set_value(true);
-                }
-                return sgl::error::no_error;
-              }),
+    Boolean_t(StringView name, bool initial_value)
+        : Button_t<LineWidth, CharT>(name,
+                                     initial_value ? StringView(impl::True<CharT>)
+                                                   : StringView(impl::False<CharT>),
+                                     &default_handle_click),
           value_(initial_value) {}
-    /**
-     * @brief Construct a boolean item with custom click handler.
-     *
-     * @tparam ClickHandler see boolean_click_handler_check for details.
-     * @param item_name name of boolean item
-     * @param text inital text of boolean item
-     * @param value initial value of boolean item
-     * @param click_handler click handler
-     */
-    template <typename ClickHandler,
-              boolean_click_handler_check<ClickHandler>* = nullptr>
-    Boolean_t(StringView     item_name,
-              StringView     text,
-              bool           value,
-              ClickHandler&& click_handler)
-        : Button_t<LineWidth, CharT>(
-              item_name,
-              text,
-              [handle = std::move(click_handler)](
-                  sgl::Button_t<LineWidth, CharT>& b) {
-                return handle(
-                    static_cast<sgl::Boolean_t<LineWidth, CharT>&>(b));
-              }),
-          value_(value) {}
 
-    bool       get_value() const { return value_; }
+    /**
+     * @brief Construct a boolean item with custom true and false strings.
+     * @param name name of item
+     * @param value inital value of item
+     * @param true_text text to set when value is true
+     * @param false_text text to set when value is false
+     */
+    Boolean_t(StringView name,
+              bool       value,
+              StringView true_text,
+              StringView false_text)
+        : Button_t<LineWidth, CharT>(name,
+                                     value ? true_text : false_text,
+                                     &default_handle_click),
+          value_(value), true_string_(true_text), false_string_(false_text) {}
+
+    bool get_value() const { return value_; }
+
     sgl::error set_value(bool value) {
       value_ = value;
+      if (value)
+        this->set_text(true_string());
+      else
+        this->set_text(false_string());
       return sgl::error::no_error;
     }
 
+    StringView true_string() const { return true_string_; }
+
+    StringView false_string() const { return false_string_; }
+
   private:
-    bool value_;
+    static sgl::error default_handle_click(Button_t<LineWidth, CharT>& button) {
+      auto& b = static_cast<Boolean_t<LineWidth, CharT>&>(button);
+      if (b.get_value()) {
+        b.set_text(b.false_string());
+        b.set_value(false);
+      } else {
+        b.set_text(b.true_string());
+        b.set_value(true);
+      }
+      return sgl::error::no_error;
+    }
+    bool       value_{false};
+    StringView true_string_{impl::True<CharT>};
+    StringView false_string_{impl::False<CharT>};
   };
 
 } // namespace sgl
