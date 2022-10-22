1eef41dc2ed9545f6751ad0076006990d7f11a55
pelectron <43130018+pelectron@users.noreply.github.com>
2022-10-22 13:05:55 +0200

    better documentation

--- include/sgl/named_value.hpp
+++ include/sgl/named_value.hpp
@@ -1,19 +1,18 @@
-
-/// \file named_value.hpp
-/// \author Pelé Constam (you\domain.com)
-/// \brief This file defines the NamedValue and Name types used by NamedTuple and operator<<=().
-/// \version 0.1
-/// \date 2022-07-19
-/// \copyright Copyright (c) 2022
+/**
+ * \file named_value.hpp
+ * \author Pelé Constam (you\domain.com)
+ * \brief This file defines the NamedValue and Name types used by NamedTuple and operator<<=().
+ * \version 0.1
+ * \date 2022-07-19
+ */
 
 //          Copyright Pele Constam 2022.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
-//
+
 #ifndef SGL_NAMED_VALUE_HPP
 #define SGL_NAMED_VALUE_HPP
-#include "sgl/index_sequence.hpp"
 #include "sgl/name.hpp"
 #include "sgl/string_view.hpp"
 
@@ -21,24 +20,25 @@
 
 namespace sgl {
 
-  /// \headerfile named_value.hpp "sgl/named_value.hpp"
-  /// \brief This type represents a pair of a sgl::Name and a value of type T.  To create a
-  /// NamedValue, either use the constructor or the \ref sgl::operator<<=(name, value). An
-  /// example of how to use:
-  ///
-  /// \code
-  ///
-  ///  auto val1 = NamedValue(NAME("N1"), 5.3f);
-  ///
-  ///  constexpr auto my_name = NAME("name");
-  ///
-  ///  auto val2 {my_name <<= 2.5f};
-  ///
-  /// \endcode
-  ///
-  /// \tparam T value type
-  /// \tparam Chars characters of the name
-  ///
+  /**
+   * \headerfile named_value.hpp "sgl/named_value.hpp"
+   * \brief This type represents a pair of a sgl::Name and a value of type T.  To create a
+   * NamedValue, either use the constructor or the \ref sgl::operator<<=(name, value). An
+   * example of how to use:
+   *
+   * \code
+   *
+   *  auto named_val1 = NamedValue(NAME("N1"), 5.3f);
+   *
+   *  constexpr auto my_name = NAME("name");
+   *
+   *  auto named_val2 {my_name <<= 2.5f};
+   *
+   * \endcode
+   *
+   * \tparam T value type
+   * \tparam Chars characters of the name
+   */
   template <typename Name, typename T>
   class NamedValue {
   public:
@@ -49,37 +49,37 @@ namespace sgl {
     /// name type of the argument
     using name_type = Name;
 
-    /// default ctor. Only participates in overload resolution if T is default constructible.
+    /// \brief default ctor.
     constexpr NamedValue() = default;
 
-    /// copy ctor
+    /// \brief copy ctor
     /// \param other object to copy
     constexpr NamedValue(const NamedValue& other) = default;
 
-    /// move ctor
+    /// \brief move ctor
     /// \param other object to move
     constexpr NamedValue(NamedValue&& other) = default;
 
-    /// construct from name and value
+    /// \brief construct from name and value
     /// \param name name of NamedValue
     /// \param value value of NamedValue
     constexpr NamedValue(Name     name,
                          const T& value) noexcept(std::is_nothrow_copy_constructible_v<T>);
 
-    /// move construct from name and value
+    /// \brief move construct from name and value
     /// \param name name of NamedValue
     /// \param value value to move into NamedValue
     constexpr NamedValue(Name name, T&& value) noexcept(std::is_nothrow_move_constructible_v<T>);
 
-    /// get name as string_view<char>
+    /// \brief get name as sgl::string_view<char>
     /// \return sgl::string_view<char>
     [[nodiscard]] constexpr sgl::string_view<char> name() const noexcept;
 
-    /// get reference to value
+    /// \brief get reference to value
     /// \return value_type&
     [[nodiscard]] constexpr T& value() noexcept;
 
-    /// get const reference to value
+    /// \brief get const reference to value
     /// \return const value_type&
     [[nodiscard]] constexpr const T& value() const noexcept;
 
@@ -87,29 +87,31 @@ namespace sgl {
     value_type value_; ///< value of the argument
   };
 
-  /// \brief template deduction guide for NamedValue
-  /// \tparam T value_type
-  /// \tparam Chars characters of name
+  /// \cond
   template <typename T, char... Chars>
   NamedValue(Name<Chars...>, T&&) -> NamedValue<Name<Chars...>, std::decay_t<T>>;
+  /// \endcond
 
-  /// \brief This operator exists for easy creation of named arguments, i.e.
-  /// \ref sgl::NamedValue<Name,T> "NamedValue". The return type of this operator is an \ref
-  /// sgl::NamedValue<Name,T> "NamedValue", if T is not another name type. The reasons for using
-  /// this operator to create a NamedValue are that is has the second lowest precedence, it is not
-  /// often used, i.e. no real danger of confusing it with anything else other than this specific
-  /// overload, and it can be implemented as a non member function.
-  ///
-  /// \code
-  /// // the type of named_value is NamedValue<Name<'n','a','m','e'>, float>
-  /// auto named_value {NAME("name") <<= 2.5f};
-  /// \endcode
-  ///
-  /// \tparam T value type
-  /// \tparam Chars characters of the name
-  /// \param name name of the NamedValue
-  /// \param value value of the NamedValue
-  /// \return  NamedValue<Name<Chars...>, std::decay_t<T>>
+  /**
+   * \brief This operator exists for easy creation of named arguments, i.e.
+   * \ref sgl::NamedValue<Name,T> "NamedValue". The return type of this operator is an \ref
+   * sgl::NamedValue<Name,T> "NamedValue", if T is not another name type. The reasons for using
+   * this operator to create a NamedValue are the following:
+   *  - it is has the second lowest precedence.
+   *  - it is not often used, i.e. no real danger of confusing it with anything else.
+   *  - it can be implemented as a non member function.
+   *
+   * \code
+   * // the type of named_value is NamedValue<Name<'n','a','m','e'>, float>
+   * auto named_value {NAME("name") <<= 2.5f};
+   * \endcode
+   *
+   * \tparam T value type
+   * \tparam Chars characters of the name
+   * \param name name of the NamedValue
+   * \param value value of the NamedValue
+   * \return  NamedValue<Name<Chars...>, std::decay_t<T>>
+   */
   template <typename T, char... Chars>
   constexpr NamedValue<Name<Chars...>, std::decay_t<T>> operator<<=(Name<Chars...> name, T&& value);
 
