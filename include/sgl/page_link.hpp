7548919a1e616d8dad8b162c667a575da0f78af8
pelectron <43130018+pelectron@users.noreply.github.com>
2022-10-23 19:03:13 +0200

    more docs

--- include/sgl/page_link.hpp
+++ include/sgl/page_link.hpp
@@ -17,64 +17,64 @@
 #include "sgl/pass_through_button.hpp"
 
 namespace sgl {
-
-  /// \ingroup item_types
-  /// \headerfile page_link.hpp "sgl/page_link.hpp"
-  /// \brief A page link is a button item, that, when clicked, causes the menu to switch to the page
-  /// specified by the link.
-  /// \tparam chars characters of the page name
-  /// \tparam TextSize text size of the link
-  /// \tparam CharT character type of the pagelink
+  /**
+   * \ingroup item_types
+   * \headerfile page_link.hpp "sgl/page_link.hpp"
+   * \brief A page link is a button item, that, when clicked, causes the menu to switch to the page
+   * specified by the link. To create a PageLink, there are three ways:
+   * \code
+   *  // version 1 : Directly from NAME macro
+   *  auto link1 = PageLink(NAME("home_page")); // link2's text is "home_page" and links to a
+   *  page named "home_page"
+   *
+   * // version 2 : from static name
+   * static constexpr auto page_to_link_to = NAME("Foo"); // defined somewhere
+   *
+   * auto link2 = PageLink(page_to_link_to); // link2's text is "Foo" and links to a page called
+   * "Foo"
+   *
+   * // or with different text
+   * auto link3 = PageLink(NAME("home_page"), "back"); // link3's text is "back" and links to a
+   * page called "home_page".
+   * \endcode
+   * \tparam chars characters of the page name
+   * \tparam TextSize text size of the link
+   * \tparam CharT character type of the pagelink
+   */
   template <char... chars, size_t TextSize, typename CharT>
-  class PageLink<Name<chars...>, TextSize, CharT>
-      : public sgl::PassThroughButton<PageLink<Name<chars...>, TextSize, CharT>> {
+  class PageLink<sgl::Name<chars...>, TextSize, CharT>
+      : public sgl::PassThroughButton<PageLink<sgl::Name<chars...>, TextSize, CharT>> {
   public:
     /// base class of PageLink
-    using Base = sgl::PassThroughButton<PageLink<Name<chars...>, TextSize, CharT>>;
+    using Base = sgl::PassThroughButton<PageLink<sgl::Name<chars...>, TextSize, CharT>>;
     /// string_view type of this item
     using StringView = typename Base::StringView;
-
-    /// \brief create a page link with the name of the page it should link to. The name will also be
-    /// used to set the links's text. The arguments type must be sgl::Name<chars...>, where
-    /// chars.. is the char sequence which makes up the page name.
-    ///
-    /// \code
-    ///  // version 1 : Directly from NAME macro
-    ///  auto link1 = PageLink(NAME("home_page")); // link2's text is "home_page" and links to a
-    ///  page named "home_page"
-    ///
-    /// // version 2 : from static name
-    /// static constexpr auto page_to_link_to = NAME("Foo"); // defined somewhere
-    ///
-    /// auto link2 = PageLink(page_to_link_to); // link2's text is "Foo" and links to a page called
-    /// "Foo"
-    ///
-    /// // or with different text
-    /// auto link3 = PageLink(NAME("home_page"), "back"); // link3's text is "back" and links to a
-    /// page called "home_page". \endcode
-    ///
-    /// \see sgl::Name for more info.
-    /// \param page_to_link name of the page to link to.
-    ///
-    explicit constexpr PageLink(Name<chars...> page_to_link) noexcept;
+    /**
+     * \brief create a page link with the \ref sgl::Name "name" of the page it should link to. The
+     * name will also be used to set the links's text. \see sgl::Name for more info. \param
+     * page_to_link name of the page to link to.
+     */
+    explicit constexpr PageLink(sgl::Name<chars...> page_to_link) noexcept;
 
     /// \brief create page link with page name to link to and text to show
     /// \param page_to_link name of page to link to.
     /// \param text text to show.
-    constexpr PageLink(Name<chars...> page_to_link, StringView text) noexcept;
-
-    /// \brief static overload of ItemBase::set_menu. The menu type cannot be a
-    /// part of the template parameters given to a page link on construction. As
-    /// such, the type of the menu and access to page changing functions can be accessed with this
-    /// helper. sgl::Menu will call set_menu() on all items in it constructors. \tparam Menu menu
-    /// type. \param menu pointer to menu instance.
+    constexpr PageLink(sgl::Name<chars...> page_to_link, StringView text) noexcept;
+    /**
+     * \brief static overload of ItemBase::set_menu.
+     * \details The menu type cannot be a part of the template parameters given to a page link on
+     * construction. As such, the type of the menu and access to page changing functions can be
+     * accessed with this helper. sgl::Menu will call set_menu() on all its pages , and as such all its items, in it constructors.
+     * \tparam Menu menu type.
+     * \param menu pointer to menu instance.
+     */
     template <typename Menu>
     constexpr void set_menu(Menu* menu) noexcept;
 
     /// get name of the page to link to.
     /// \return sgl::string_view<char>
     [[nodiscard]] constexpr sgl::string_view<char> page_name() const noexcept {
-      return Name<chars...>::view;
+      return sgl::Name<chars...>::view;
     }
 
     /// get pointer to menu as void*
@@ -87,11 +87,11 @@ namespace sgl {
 
   /// \cond
   template <char... chars, size_t TextSize, typename CharT>
-  PageLink(Name<chars...> page_name, const CharT (&text)[TextSize])
-      -> PageLink<Name<chars...>, TextSize - 1, CharT>;
+  PageLink(sgl::Name<chars...> page_name, const CharT (&text)[TextSize])
+      -> PageLink<sgl::Name<chars...>, TextSize - 1, CharT>;
 
   template <char... chars>
-  PageLink(Name<chars...> page_name) -> PageLink<Name<chars...>, sizeof...(chars), char>;
+  PageLink(sgl::Name<chars...> page_name) -> PageLink<sgl::Name<chars...>, sizeof...(chars), char>;
   /// \endcond
 
   /// \ingroup item_factories
@@ -101,8 +101,8 @@ namespace sgl {
   /// \brief create a pagelink item with only the name of the page to link to.
   /// \param page_name name of the page to link to.
   template <char... chars>
-  constexpr PageLink<Name<chars...>, sizeof...(chars), char>
-      make_pagelink(Name<chars...> page_name);
+  constexpr PageLink<sgl::Name<chars...>, sizeof...(chars), char>
+      make_pagelink(sgl::Name<chars...> page_name);
 
   /// \brief create a pagelink with name and text literal. Name, Character type and textsize are
   /// automatically deduced.
