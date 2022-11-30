b24fa9654a8d3898e856d9d9cc450911fb52edd1
Pelé <pele.constam@gmail.com>
2022-11-30 08:30:32 +0100

    fixes in cx_arg

--- include/sgl/cx_arg.hpp
+++ include/sgl/cx_arg.hpp
@@ -2,12 +2,13 @@
 /// @author Pelé Constam(pelectron1602 @gmail.com)
 /// @version 0.1
 /// @date 2022 - 09 -24
-//
-//          Copyright Pele Constam 2022.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
-//
+///
+/// @copyright
+///              Copyright Pele Constam 2022.
+/// Distributed under the Boost Software License, Version 1.0.
+///    (See accompanying file LICENSE_1_0.txt or copy at
+///          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef SGL_CX_ARG_HPP
 #define SGL_CX_ARG_HPP
 #include "sgl/format.hpp"
@@ -44,18 +45,19 @@ namespace sgl {
 
     /**
       unary plus operator for same reason as negation operator,
-      just that this time the value does not change.@ return cx_arg<T, N>
+      just that this time the value does not change.
+      @return cx_arg<T, N>
     */
     constexpr cx_arg<T, N> operator+() { return *this; }
   };
 
   namespace cx_arg_literals {
-
     /**
       create cx_arg double literal.
       @note This operator merely parses a double out of of the chars given and returns a cx_arg
       containing its value and a string of it **as it was declared**. Essentially, you, the
       programmer, are the compile time formatter. See the example below for more info.
+
       ```cpp
       #include "sgl_cx_arg.hpp"
       using namespace sgl::cx_arg_literals;
@@ -69,7 +71,7 @@ namespace sgl {
     */
     template <char... chars>
     constexpr cx_arg<double, sizeof...(chars)> operator"" _double() {
-      constexpr char                   arr[sizeof...(chars) + 1]{chars..., '\n'};
+      constexpr char                   arr[sizeof...(chars) + 1]{chars..., '\0'};
       cx_arg<double, sizeof...(chars)> ret{0.0, static_string{arr}};
       ret.value = sgl::parse_impl::parse<double>(ret.string.data(), sizeof...(chars));
       return ret;
@@ -89,7 +91,7 @@ namespace sgl {
     */
     template <char... chars>
     constexpr cx_arg<float, sizeof...(chars)> operator"" _float() {
-      constexpr char                  arr[sizeof...(chars) + 1]{chars..., '\n'};
+      constexpr char                  arr[sizeof...(chars) + 1]{chars..., '\0'};
       cx_arg<float, sizeof...(chars)> ret{0.0f, static_string{arr}};
       ret.value =
           static_cast<float>(sgl::parse_impl::parse<float>(ret.string.data(), sizeof...(chars)));
